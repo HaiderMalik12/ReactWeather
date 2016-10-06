55023d31d8ad6acafb9712b2f01e1ea8ace57369
HaiderMalik12 <haidermalik504@gmail.com>
2016-10-07 02:19:17 +0500

    Added https support

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const express = require('express');
 
 //create our App
 const app = express();
-app.use(function(req,res,next){
-    if(req.headers['x-forwarded-proto'] === 'http'){
-    next();
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + req.url);
+
     } else {
-   res.redirect('http://'+req.hostname + req.url);
+        next();
     }
 });
-const PORT =process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(PORT,() => {
-  console.log('Express server is running on PORT 3000');
+app.listen(PORT, () => {
+    console.log('Express server is running on PORT 3000');
 });

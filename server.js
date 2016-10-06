627f6148a6207305c8eabe54e344737724cab76d
HaiderMalik12 <haidermalik504@gmail.com>
2016-10-07 02:01:13 +0500

    Add code for production

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 //create our App
 const app = express();
+app.use(function(req,res,next){
+    if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+    } else {
+   res.redirect('http://'+req.hostname + req.url);
+    }
+});
+const PORT =process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000,() => {
+app.listen(PORT,() => {
   console.log('Express server is running on PORT 3000');
 });
